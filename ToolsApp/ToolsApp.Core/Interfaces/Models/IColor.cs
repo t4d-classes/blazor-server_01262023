36af3357734aa31a9e01308437671d2386490f3f
Eric Greene <eric@t4d.io>
2023-01-27 12:07:41 -0500

    add exercise 5

--- ToolsApp/ToolsApp.Core/Interfaces/Models/IColor.cs
+++ ToolsApp/ToolsApp.Core/Interfaces/Models/IColor.cs
@@ -1,15 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ToolsApp.Core.Interfaces.Models
+﻿namespace ToolsApp.Core.Interfaces.Models
 {
-  public interface IColor
+  public interface INewColor
   {
-    int Id { get; set; }
     string Name { get; set; }
     string HexCode { get; set; }
+  }
+
+  public interface IColor: INewColor
+  {
+    int Id { get; set; }
   }
 }
